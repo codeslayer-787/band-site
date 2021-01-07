19abcbf31a97fc7c0b66caa39e0fd7b709364181
codeslayer-787 <irwin_ayala@yahoo.com>
2021-01-07 15:26:42 -0600

    Add Javascript

--- app1.js
+++ app1.js
@@ -1,2 +1,9 @@
-let instrument = prompt('What Instrument Will You Play?');
-document.write(instrument)
+function askInstrument(){
+    // Added functions on line 1 and 5
+    let instrument = prompt('What Instrument Will You Play?');
+    document.write(instrument);
+    return instrument;
+    // Added line 8 invocation 
+}
+let instrument = askInstrument();
+
---
+++ app4.js
@@ -0,0 +1,9 @@
+function replaceimage(){
+    if (instrument == 'none'){
+    document.getElementById("dave").src="https://consequenceofsound.net/wp-content/uploads/2018/12/limp-bizkit.png"
+}
+else {
+    document.getElementById("dave").src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Dave_Matthews_Band_-_Band_Shot_Melbourne_2005.jpg"
+}
+}
+replaceimage();
