eb780884f95c07c4a3a7c3453122cf1875138540
codeslayer-787 <irwin_ayala@yahoo.com>
2021-01-07 19:19:16 -0600

    Add loops

--- app3.js
+++ app3.js
@@ -1,3 +1,24 @@
-if (instrument == 'Bass'){
-    document.write('<h3>' + 'https://codeslayer-787.github.io/reading-notes/' + '</h3>');
+
+let correctAnswer = 1;
+
+function bandMembers(){
+    let userAnswer = prompt ('How many guitars does a band need?');
+    while (userAnswer < 1 || userAnswer > 10){
+        userAnswer = prompt('Too What?  Please select a number 1 - 10');
+    }
+    let numberOfGuesses = 4
+    for(let i = 0; i <numberOfGuesses; i++){
+        if (userAnswer == correctAnswer){
+            alert('Great Job!  You got it right.');            
+        } else if (userAnswer < correctAnswer){
+            alert('Too Few.  Please select a number 1 - 10');
+        } else if (userAnswer > correctAnswer){
+            alert('Too Many!  Do you want to make music or just a lot of noise?');
+            userAnswer = prompt('Please select a number 1 - 10');
+        }
+    }
 }
+let link = document.getElementById('members');
+link.addEventListener('click', bandMembers);
+
+
