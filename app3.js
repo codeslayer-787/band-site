c5a9c92c941be494844d9c37f83790fb5850bfa2
codeslayer-787 <irwin_ayala@yahoo.com>
2021-01-07 19:35:48 -0600

    Fix Links

--- app3.js
+++ app3.js
@@ -3,15 +3,17 @@ let correctAnswer = 1;
 
 function bandMembers(){
     let userAnswer = prompt ('How many guitars does a band need?');
-    while (userAnswer < 1 || userAnswer > 10){
+    while (userAnswer < 0 || userAnswer > 10){
         userAnswer = prompt('Too What?  Please select a number 1 - 10');
     }
     let numberOfGuesses = 4
     for(let i = 0; i <numberOfGuesses; i++){
         if (userAnswer == correctAnswer){
-            alert('Great Job!  You got it right.');            
+            alert('Great Job!  You got it right.');
+            break;            
         } else if (userAnswer < correctAnswer){
             alert('Too Few.  Please select a number 1 - 10');
+            userAnswer = prompt('Please select a number 1 - 10');
         } else if (userAnswer > correctAnswer){
             alert('Too Many!  Do you want to make music or just a lot of noise?');
             userAnswer = prompt('Please select a number 1 - 10');
@@ -20,5 +22,3 @@ function bandMembers(){
 }
 let link = document.getElementById('members');
 link.addEventListener('click', bandMembers);
-
-
